31e6385a047955383b97466a4a29d757f2f6e2d9
Sarvesh Kalwit <sarvesh.kalwit@utexas.edu>
2019-11-14 20:33:13 -0600

    fixed some formatting issues

--- app.py
+++ app.py
@@ -2,9 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/home')
 def index():
 	return render_template('index.html')
+
+@app.route('/mailing')
+def mailing():
+	return render_template('mailing.html')
+
+@app.route('/quote.txt')
+def quote():
+	return render_template('quote.txt')
 
 if __name__ == '__main__':
 	app.run(debug=True)
