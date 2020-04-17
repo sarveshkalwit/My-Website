e18b5f784d7d2ee1a3b030b1840905bdf9da15cf
Sarvesh Kalwit <sarvesh.kalwit@utexas.edu>
2020-04-17 12:10:27 -0700

    created external link component

--- my_site/src/App.js
+++ my_site/src/App.js
@@ -1,12 +1,46 @@
 import React, { Component } from 'react';
+import {
+	BrowserRouter as Router,
+	Link,
+	Route,
+	Switch,
+} from 'react-router-dom';
 import './App.css';
+
+import Github from "./images/github.svg";
 
 class App extends Component {
 	render() {
 		return (
 			<div>
 				<h1>Hello World!</h1>
+				<ExternalLink
+					href="https://www.github.com/sarveshkalwit"
+					src={ Github }
+					alt="Github"
+				/>
 			</div>
+		);
+	}
+}
+
+class ExternalLink extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<Router>
+				<Link to={ this.props.href }>
+					<img
+						src={ this.props.src }
+						alt={ this.props.name }
+						width="20px"
+						height="20px"
+					/>
+				</Link>
+			</Router>
 		);
 	}
 }
