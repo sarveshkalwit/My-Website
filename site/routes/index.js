3cae3e5167731ef2e86ef9ddbb03600485ff6515
Sarvesh Kalwit <sarvesh.kalwit@utexas.edu>
2020-06-15 02:51:24 -0700

    lots of changes

--- site/routes/index.js
+++ site/routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
+const Alpaca = require('@alpacahq/alpaca-trade-api')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	// Github API calls
+		// https://stackoverflow.com/questions/21869795/github-api-retrieve-user-commits
+	
+	// Alpaca API calls
+	const startDate = "";
+	const todayDate = "";
+	const alpaca = new Alpaca({
+		keyId: process.env.APCA_API_KEY_ID,
+		secretKey: process.env.APCA_API_SECRET_KEY,
+		paper: true,
+		usePolygon: false
+	})
+	alpaca.getPortfolioHistory({
+	])
+		
+
+	res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
