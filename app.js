299e2b24045617a8558f1c9f5844a219beb9cdc8
Sarvesh Kalwit <sarvesh.kalwit@utexas.edu>
2020-08-05 19:32:37 -0700

    minor bug fix

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser(urlencoded({extended: true})));
+app.use(bodyParser(express.urlencoded({extended: true})));
 
 app.use('/', indexRouter);
 app.use('/connect', connectRouter);
